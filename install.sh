8edc5b3b3d0fec28484996af181a5b25f3950472
Mustafa Arınmış <56651041+Arinmis@users.noreply.github.com>
2021-11-30 22:15:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,8 @@
 sudo apt install vim-gtk3  
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/Arinmis/vim-settings.git
 
 # install runtime configs
-cd vim-settings
 cp vimrc ~ 
 mv ~/vimrc ~/.vimrc
 echo Setup compeleted!!!
